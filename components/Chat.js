133a458bedb642a0236f5c50a0ba8f883f0ace90
mattig94 <mattig94@users.noreply.github.com>
2020-03-19 17:46:30 -0400

    Basics of chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
 //main chat view
 export default class Chat extends Component {
-  //put users name in nav bar
+  state = {
+    messages: [],
+  }
+
+  //puts user's name in nav bar
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.state.params.name
     };
   };
 
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello Developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: this.props.navigation.state.params.name + ' entered the chat',
+          createdAt: new Date(),
+          system: true,
+        }
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
   render() {
     return (
       <View style={[styles.container, {backgroundColor: this.props.navigation.state.params.color}]}>
         <Text style={{color: '#FFFFFF'}}>Chat</Text>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardSpacer/> : null }
       </View>
     );
   }
@@ -22,8 +65,6 @@ export default class Chat extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: '#FFF',
   },
 });
