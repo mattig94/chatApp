cccb52ebd027489c42a77c767152688065a62fb1
mattig94 <mattig94@users.noreply.github.com>
2020-03-30 17:37:31 -0400

    integrating firebase

--- components/Chat.js
+++ components/Chat.js
@@ -3,10 +3,38 @@ import { StyleSheet, Text, View, Platform } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 
+//use to fix base64 error
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
+//import firestore
+const firebase = require('firebase');
+require('firebase/firestore');
+
 //main chat view
 export default class Chat extends Component {
-  state = {
-    messages: [],
+  constructor() {
+    super()
+    //connect to firestore
+    if (!firebase.apps.length){
+      firebase.initializeApp({
+        apiKey: "AIzaSyCm7rBTAfptfThXjWQhkp_H2B4yJ94Csbk",
+        authDomain: "chat-app-9ef43.firebaseapp.com",
+        databaseURL: "https://chat-app-9ef43.firebaseio.com",
+        projectId: "chat-app-9ef43",
+        storageBucket: "chat-app-9ef43.appspot.com",
+        messagingSenderId: "430643467945",
+        appId: "1:430643467945:web:647c27b024b1ed29e13bb8",
+        measurementId: "G-E4XCFDT292"
+      });
+    }
+    //refernce collection
+    this.referenceMessages = firebase.firestore().collection('messages');
+    //get and store messages for chat
+    state = {
+      messages: [],
+    }
   }
 
   //puts user's name in nav bar
@@ -17,6 +45,16 @@ export default class Chat extends Component {
   };
 
   componentDidMount() {
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        loggedInText: 'Hello there',
+      });
+      this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
+    });
     this.setState({
       messages: [
         {
@@ -39,10 +77,47 @@ export default class Chat extends Component {
     })
   }
 
+  componentWillUnmount() {
+    this.authUnsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    querySnapshot.forEach((doc) => {
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        createdAt: data.createdAt,
+        text: data.text,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  addMessage() {
+    this.referenceMessages.add({
+      _id: this.state.messages[0]._id,
+      text: this.state.messages[0].text,
+      createdAt: this.state.messages[0].createdAt,
+      user: this.state.messages[0].user,
+      uid: this.state.uid,
+    });
+  };
+
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    }),
+      () => {
+        this.addMessage();
+      })
   }
 
   render() {
